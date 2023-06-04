f7da9a156a67dda84b84d809644063185060280b
hkstwk <hkolvoort@gmail.com>
2023-06-04 18:13:22 +0200

    initial commit, starting point from spring initialzr

---
+++ src/main/java/nl/hkstwk/spring/SpringGuidePayrollRestService/SpringGuidePayrollRestServiceApplication.java
@@ -0,0 +1,13 @@
+package nl.hkstwk.spring.SpringGuidePayrollRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuidePayrollRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuidePayrollRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/hkstwk/spring/SpringGuidePayrollRestService/SpringGuidePayrollRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hkstwk.spring.SpringGuidePayrollRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuidePayrollRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
